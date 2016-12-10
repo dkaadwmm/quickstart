115c68c168063b651543bc0339d0b22d6b7fa7ef
Moritz <dkaadwmm@gmail.com>
2016-12-10 16:40:44 +0100

    hero detail is reuseable

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -74,18 +72,11 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
     </ul>
-    <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name">
-    </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `
   ,
 })
---
+++ app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Moritz on 09.12.16.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
